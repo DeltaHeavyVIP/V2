665f42dc032652ef2c8acbd87c4e9baaae2b102b
unknown <patutin.vovan@mail.ru>
2021-03-26 12:01:41 +0300

    met 5

--- src/main/java/methods/Method_5.java
+++ src/main/java/methods/Method_5.java
@@ -1,6 +1,11 @@
 package methods;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Stack;
+
+import static methods.Function.function;
+import static methods.Function.second_derivative;
 
 public class Method_5 {
 
@@ -12,6 +17,7 @@ public class Method_5 {
     private String file_or_console;
     private int loop = 0;
     private double lambda;
+    private String answer = "";
 
     private Stack<Double> stack_function_x = new Stack<>();
     private Stack<Double> stack_function_fi_x = new Stack<>();
@@ -27,65 +33,96 @@ public class Method_5 {
         this.file_or_console = file_or_console;
     }
 
-    public void do_it() {
-
-        double q;
-        if (derivative(a) >= derivative(b)) {
-            lambda = -1 / derivative(a);
-        } else {
-            lambda = -1 / derivative(b);
-        }
-
-        stack_x.push(0.0);
-        get_x_0();
-
-        if (Math.abs(derivative(a)) >= Math.abs(derivative(b))) {
-            q = Math.abs(1 + lambda * (derivative(a)));
-        } else {
-            q = Math.abs(1 + lambda * (derivative(b)));
-        }
-        if (q > 0.5 && q < 1) {
-            e = (1 - q) * e / q;
-        }
-
-        if (q < 1) {
-            while (Math.abs(x - stack_x.peek()) > e) {
-                System.out.println(x);
-                stack_x.push(x);
-                stack_function_x.push(function(x));
-                stack_function_fi_x.push(get_x(x));
-                get_x(x);
-                stack_new_x.push(x);
-                stack_e.push(Math.abs(x - stack_x.peek()));
-                loop++;
-            }
-        }
-        //TODO System.out.println("Коэффициент сходимости превышает 1: q = " + q");
-    }
-
-
     private double get_x_0() {
-        if (function(b) * second_derivative(b) >= 0) {
+        if (function(b, equation) * second_derivative(b, equation) >= 0) {
             x = b;
-        } else if (function(a) * second_derivative(a) >= 0) {
+        } else {
             x = a;
         }
         return x;
     }
 
-    private double get_x(double x) {
-        return x + lambda * function(x);
+    private double Fi() {
+        return x + lambda * Function.function(x, equation);
     }
 
-    private double function(double x) {
-        return x * x * x - 4.5 * x * x - 9.21 * x - 0.383;
+    public double Fi_(double x) {
+        return 1 + lambda * Function.derivative(x, equation);
     }
 
-    private double derivative(double x) {
-        return 3 * x * x - 9 * x - 9.21;
+    public double getMaxQ(double a, double b) {
+        return Math.max(Math.abs(Fi_(a)), Math.abs(Fi_(b)));
     }
 
-    private double second_derivative(double x) {
-        return 6 * x - 9;
+    public static double getMaxF(double a, double b, int number) {
+        return Math.max(Function.derivative(a, number), Function.derivative(b, number));
+    }
+
+    public void do_it() {
+        double q = 0.0;
+
+        lambda = -1 / getMaxF(a, b, equation);
+        stack_x.add(get_x_0());
+        x = Fi();
+        stack_new_x.add(x);
+        stack_function_x.add(Function.function(x,equation));
+        stack_function_fi_x.add(Fi());
+        stack_e.add(Math.abs(x-stack_x.get(loop)));
+        q = getMaxQ(a, b);
+
+
+        if (q < 1) {
+            if (q > 0.5 && q < 1) {
+                e = (1 - q) * e / q;
+            }
+
+            while (stack_e.get(loop) > e) {
+                stack_x.add(x);
+                x = Fi();
+                stack_new_x.add(x);
+                stack_function_x.add(Function.function(x,equation));
+                stack_function_fi_x.add(Fi());
+                stack_e.add(Math.abs(x-stack_x.get(loop)));
+                loop++;
+            }
+            //TODO out
+            if (file_or_console.equals("console")) {
+                System.out.println("+-----------------------------------------------------------------------------+");
+                System.out.printf("|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s|\n",
+                        "№", "xk", "f(xk)", "f'(xk)", "xk+1","|xk - xk+1|");
+                for (int i = 0; i < loop+1; i++) {
+                    System.out.println("|------------+------------+------------+" +
+                            "------------+------------+------------|");
+                    System.out.printf("|%-12s|%-12.3f|%-12.3f|%-12.3f|%-12.3f|%-12.3f|\n",
+                            (i+1), stack_x.get(i+1), stack_function_x.get(i), stack_derivative_x.get(i),stack_new_x.get(i),stack_e.get(i));
+                }
+                System.out.println("+-----------------------------------------------------------------------------+");
+            } else {
+                answer = "+-----------------------------------------------------------------------------+\n";
+                answer += String.format("|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s|\n",
+                        "№", "xk", "f(xk)", "f'(xk)", "xk+1","|xk - xk+1|");
+                for (int i = 0; i < loop+1; i++) {
+                    answer += String.format("|------------+------------+------------+" +
+                            "------------+------------+------------|\n");
+                    answer += String.format("|%-12s|%-12.3f|%-12.3f|%-12.3f|%-12.3f|%-12.3f|\n",
+                            (i+1), stack_x.get(i+1), stack_function_x.get(i), stack_derivative_x.get(i),stack_new_x.get(i),stack_e.get(i));
+                }
+                answer += String.format("+-----------------------------------------------------------------------------+\n");
+            }
+        } else {
+            // TODO System.out.println("\nКоэффициент сходимости превышает 1: q = " + q);
+            if (file_or_console.equals("console")) {
+                System.out.println("+-----------------------------------------------------------------------------+");
+            }else{
+                String answer = "+-----------------------------------------------------------------------------+\n";
+            }
+            }
+        try(FileWriter writer = new FileWriter("src/main/resources/output_1", false)){
+            writer.write(answer);
+            writer.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        }
     }
 }
